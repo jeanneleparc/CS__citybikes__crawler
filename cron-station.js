c7a3fc90c9b27958b68c140f08420da8614a3670
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-01-05 15:03:01 +0100

    ajout d'un premier appel à information au lancement du crawler

--- cron-station.js
+++ cron-station.js
@@ -18,6 +18,33 @@ async function get(url) {
 }
 const AMQP_URL = process.env.AMQP_URL || 'amqp://localhost';
 
+amqp.connect(AMQP_URL, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(async function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        const url = `https://gbfs.citibikenyc.com/gbfs/en/station_information.json`;
+        const stations = await get(url);
+        const stations_stringify = JSON.stringify(stations);
+
+        var queue = 'information';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue,  Buffer.from(stations_stringify));
+        console.log("Information Send");
+    });
+    // setTimeout(function() {
+    //     connection.close();
+    //     process.exit(0);
+    // }, 10000);
+});
+
 cron.schedule('*/10 * * * *', function() {
     amqp.connect(AMQP_URL, function(error0, connection) {
         if (error0) {
