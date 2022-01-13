6189b1e58e6a0ed65b918dd47370d272da70acee
BOUYSSEL Camille <45000497+BouysselCamille@users.noreply.github.com>
2022-01-13 15:34:54 +0100

    Circleci project setup (#3)
    * Add .circleci/config.yml
    
    * add prettier and eslint
    
    * git hook

--- cron-station.js
+++ cron-station.js
@@ -1,78 +1,96 @@
 // #!/usr/bin/env node
 
-const cron = require('node-cron');
-const amqp = require('amqplib/callback_api');
-const XMLHttpRequest = require('xhr2');
+const cron = require("node-cron");
+const amqp = require("amqplib/callback_api");
+const XMLHttpRequest = require("xhr2");
 
 async function get(url) {
   return new Promise((resolve) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", url, true);
-      xhr.responseType = "json";
-      xhr.onload = () => resolve(xhr.response);
-      xhr.send(null);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = () => resolve(xhr.response);
+    xhr.send(null);
   });
 }
-const AMQP_URL = process.env.AMQP_URL || 'amqp://localhost';
-const url_stations = `https://gbfs.citibikenyc.com/gbfs/en/station_information.json`;
-const url_status = `https://gbfs.citibikenyc.com/gbfs/en/station_status.json`;
+const AMQP_URL = process.env.AMQP_URL || "amqp://localhost";
+const urlStations = `https://gbfs.citibikenyc.com/gbfs/en/station_information.json`;
+const urlStatus = `https://gbfs.citibikenyc.com/gbfs/en/station_status.json`;
 
 // fetch the information of the stations to fill the database
-amqp.connect(AMQP_URL, function(error0, connection) {
-    if (error0) { throw error0; }
-    connection.createChannel(async function(error1, channel) {
-        if (error1) { throw error1; }
+amqp.connect(AMQP_URL, (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(async (error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
 
-        const stations = await get(url_stations);
-        const stations_stringify = JSON.stringify(stations);
+    const stations = await get(urlStations);
+    const stationsStringify = JSON.stringify(stations);
 
-        var queue = 'information';
+    const queue = "information";
 
-        channel.assertQueue(queue, { durable: false });
-        channel.sendToQueue(queue,  Buffer.from(stations_stringify));
-        console.log("Information Send");
-    });
-    setTimeout(function() { connection.close(); }, 10000);
+    channel.assertQueue(queue, { durable: false });
+    channel.sendToQueue(queue, Buffer.from(stationsStringify));
+    console.log("Information Send");
+  });
+  setTimeout(() => {
+    connection.close();
+  }, 10000);
 });
 
 // schedule a cron task to fetch the information of the stations every 10min
-cron.schedule('*/10 * * * *', function() {
-    amqp.connect(AMQP_URL, function(error0, connection) {
-        if (error0) { throw error0; }
-        connection.createChannel(async function(error1, channel) {
-            if (error1) { throw error1; }
-    
-            const stations = await get(url_stations);
-            const stations_stringify = JSON.stringify(stations);
-    
-            var queue = 'information';
-    
-            channel.assertQueue(queue, { durable: false });
-            channel.sendToQueue(queue,  Buffer.from(stations_stringify));
-            console.log("Information Send");
-        });
-        setTimeout(function() { connection.close(); }, 10000);
+cron.schedule("*/10 * * * *", () => {
+  amqp.connect(AMQP_URL, (error0, connection) => {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(async (error1, channel) => {
+      if (error1) {
+        throw error1;
+      }
+
+      const stations = await get(urlStations);
+      const stationsStringify = JSON.stringify(stations);
+
+      const queue = "information";
+
+      channel.assertQueue(queue, { durable: false });
+      channel.sendToQueue(queue, Buffer.from(stationsStringify));
+      console.log("Information Send");
     });
-    console.log('Fetch information');
+    setTimeout(() => {
+      connection.close();
+    }, 10000);
   });
+  console.log("Fetch information");
+});
 
 // schedule a cron task to fetch the status of the stations every min
-cron.schedule('* * * * *', function() {
-    amqp.connect(AMQP_URL, function(error0, connection) {
-        if (error0) { throw error0; }
-        connection.createChannel(async function(error1, channel) {
-            if (error1) { throw error1; }
+cron.schedule("* * * * *", () => {
+  amqp.connect(AMQP_URL, (error0, connection) => {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(async (error1, channel) => {
+      if (error1) {
+        throw error1;
+      }
 
-            const status = await get(url_status);
-            const status_stringify = JSON.stringify(status);
+      const status = await get(urlStatus);
+      const stationsStringify = JSON.stringify(status);
 
-            var queue = 'status';
+      const queue = "status";
 
-            channel.assertQueue(queue, { durable: false });
-            channel.sendToQueue(queue,  Buffer.from(status_stringify));
-            console.log("Status Send");
-        });
-        setTimeout(function() { connection.close(); }, 10000);
+      channel.assertQueue(queue, { durable: false });
+      channel.sendToQueue(queue, Buffer.from(stationsStringify));
+      console.log("Status Send");
     });
-    console.log('Fetch status');
+    setTimeout(() => {
+      connection.close();
+    }, 10000);
   });
+  console.log("Fetch status");
+});
--- send_status.js
+++ send_status.js
@@ -1,38 +1,41 @@
 // #!/usr/bin/env node
-const amqp = require('amqplib/callback_api');
-const XMLHttpRequest = require('xhr2');
-
+const amqp = require("amqplib/callback_api");
+const XMLHttpRequest = require("xhr2");
 
 // Quick wrapper function for making a GET call.
 async function get(url) {
-    return new Promise((resolve) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.responseType = "json";
-        xhr.onload = () => resolve(xhr.response);
-        xhr.send(null);
-    });
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = () => resolve(xhr.response);
+    xhr.send(null);
+  });
 }
-const AMQP_URL = process.env.AMQP_URL || 'amqp://localhost';
+const AMQP_URL = process.env.AMQP_URL || "amqp://localhost";
 
 // connection to rabbitMQ and fetch status data
-amqp.connect(AMQP_URL, function(error0, connection) {
-    if (error0) { throw error0; }
-    connection.createChannel(async function(error1, channel) {
-        if (error1) { throw error1; }
+amqp.connect(AMQP_URL, (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(async (error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
 
-        const url = `https://gbfs.citibikenyc.com/gbfs/en/station_status.json`;
-        const status = await get(url);
-        const status_stringify = JSON.stringify(status);
+    const url = `https://gbfs.citibikenyc.com/gbfs/en/station_status.json`;
+    const status = await get(url);
+    const stationsStringify = JSON.stringify(status);
 
-        var queue = 'status';
+    const queue = "status";
 
-        channel.assertQueue(queue, { durable: false });
-        channel.sendToQueue(queue,  Buffer.from(status_stringify));
-        console.log("Status Send");
-    });
-    setTimeout(function() {
-        connection.close();
-        process.exit(0);
-    }, 10000);
+    channel.assertQueue(queue, { durable: false });
+    channel.sendToQueue(queue, Buffer.from(stationsStringify));
+    console.log("Status Send");
+  });
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 10000);
 });
