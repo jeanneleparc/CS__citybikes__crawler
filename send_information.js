91aa1ffee28c41d458694abbb1cf11d50727c701
Bouyssel Camille <camille.bouyssel@student-cs.fr>
2022-01-13 11:12:01 +0100

    add prettier and eslint

--- send_information.js
+++ send_information.js
@@ -1,40 +1,42 @@
 // #!/usr/bin/env node
-const amqp = require('amqplib/callback_api');
-const XMLHttpRequest = require('xhr2');
-
+const amqp = require("amqplib/callback_api");
+const XMLHttpRequest = require("xhr2");
 
 // Quick wrapper function for making a GET call.
 async function get(url) {
-    return new Promise((resolve) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.responseType = "json";
-        xhr.onload = () => resolve(xhr.response);
-        xhr.send(null);
-    });
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = () => resolve(xhr.response);
+    xhr.send(null);
+  });
 }
 
-const AMQP_URL = process.env.AMQP_URL || 'amqp://localhost';
-
+const AMQP_URL = process.env.AMQP_URL || "amqp://localhost";
 
 // connection to rabbitMQ and fetch data
-amqp.connect(AMQP_URL, function(error0, connection) {
-    if (error0) { throw error0; }
-    connection.createChannel(async function(error1, channel) {
-        if (error1) { throw error1; }
+amqp.connect(AMQP_URL, (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(async (error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
 
-        const url = `https://gbfs.citibikenyc.com/gbfs/en/station_information.json`;
-        const stations = await get(url);
-        const stations_stringify = JSON.stringify(stations);
+    const url = `https://gbfs.citibikenyc.com/gbfs/en/station_information.json`;
+    const stations = await get(url);
+    const stationsStringify = JSON.stringify(stations);
 
-        var queue = 'information';
+    const queue = "information";
 
-        channel.assertQueue(queue, { durable: false });
-        channel.sendToQueue(queue,  Buffer.from(stations_stringify));
-        console.log("Information Send");
-    });
-    setTimeout(function() {
-        connection.close();
-        process.exit(0);
-    }, 10000);
+    channel.assertQueue(queue, { durable: false });
+    channel.sendToQueue(queue, Buffer.from(stationsStringify));
+    console.log("Information Send");
+  });
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 10000);
 });
